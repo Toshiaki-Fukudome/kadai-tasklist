13c521689301a285760975743e9fa4482fd0aaec
toshiaki.fukudome <chiri.fukudome@gmail.com>
2018-08-20 10:26:56 +0000

    add correct(comp)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in
-  before_action :corrent_user
+  before_action :require_user_logged_in, except: [:index]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     #ページネーションの追加
